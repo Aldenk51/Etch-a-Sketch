27368f3aaaa23834e1f145780924570edf9ea922
Alden Kim <aldenk51@gmail.com>
2023-03-10 18:39:27 -0500

    Added change size functionality and touch ups

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,24 @@
 /**
- * Create a grid of boxes with eventListeners hover effect
+ * Initialize
  */
 let mode = "color";
-let toAdd = document.createDocumentFragment();
-for(let i=0; i < 16; i++){
-  let newDiv = document.createElement('div');
-  newDiv.style.backgroundColor = 'rgb(255, 255, 255';
-  newDiv.id = "box"+i;
-  newDiv.className = "grid"
-  newDiv.addEventListener('mouseover', e => {
-    switch (mode) {
-      case "color" :
-        e.target.style.backgroundColor = 'rgb(82, 82, 82)';
-        break;
-      
-      case "rgb" :
-        e.target.style.backgroundColor = randomColor();
-        break;
-
-      case "darken" :
-        e.target.style.backgroundColor = darkenColor(...e.target.style.backgroundColor.match(/\d+/g))
-        break;
-
-      default:
-        break;
-    }
-   });
-   toAdd.appendChild(newDiv);
-}
-document.querySelector('#container').appendChild(toAdd);
-
-/**
- * Adds event listener to all buttons via containing DIV
- */
+createGrid(4);
+// Adds event listener to all buttons via containing DIV
 document.querySelector('#options').addEventListener('click', e => {
   if(e.target.id == 'setSize') {
-
-  } else {
+    let i = prompt("How many blocks per side:");
+    if(i) {
+      createGrid(i)
+    }
+  } else if(e.target.id != 'options'){
     mode = e.target.id;
+    console.log(e.target.id);
   }
 });
 
+
 /**
- * Generates a random color 0-255 for each rgb value
+ * Generates a random value for each rgb value
  */
 function randomColor() {
   let color = "#";
@@ -53,6 +29,7 @@ function randomColor() {
   return color;
 }
 
+
 /**
  * @param {Number} r - Red value of color
  * @param {Number} g - Green value of color
@@ -60,5 +37,45 @@ function randomColor() {
  * @returns Value of darkened color provided
  */
 function darkenColor(r, g, b) {
-  return 'rgb(' + r*0.8 + ', ' + g*0.8 + ', ' + b*0.8 + ')';
+  return 'rgb(' + r*0.7 + ', ' + g*0.7 + ', ' + b*0.7 + ')';
+}
+
+
+/**
+ * Create a grid of boxes with eventListeners hover effect
+ */
+function createGrid(size) {
+  let toAdd = document.createDocumentFragment();
+  for(i = 0; i < size**2; i ++) {
+    let newDiv = document.createElement('div');
+    newDiv.style.backgroundColor = 'rgb(255, 255, 255';
+    newDiv.id = "box"+i;
+    newDiv.className = "grid"
+    newDiv.addEventListener('mouseover', e => {
+      switch (mode) {
+        case "color" :
+          e.target.style.backgroundColor = 'rgb(82, 82, 82)';
+          break;
+        
+        case "rgb" :
+          e.target.style.backgroundColor = randomColor();
+          break;
+  
+        case "darken" :
+          e.target.style.backgroundColor = darkenColor(...e.target.style.backgroundColor.match(/\d+/g))
+          break;
+  
+        default:
+          break;
+      }
+     });
+     toAdd.appendChild(newDiv);
+  }
+  const container = document.createElement('div');
+  container.id = "container";
+  container.appendChild(toAdd);
+
+  document.body.replaceChild(container, document.querySelector('#container'));
+
+  document.querySelector('#container').style.gridTemplateColumns = " auto".repeat(size);
 }
