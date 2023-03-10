bb767e5341d373af17776ec3b3ae2c7d2d145cb1
Alden Kim <aldenk51@gmail.com>
2023-03-09 23:32:46 -0500

    Updated eventListener to be added during creation of div

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,14 @@
 /**
- * Create a grid of boxes
+ * Create a grid of boxes with eventListeners hover effect
  */
 let toAdd = document.createDocumentFragment();
 for(let i=0; i < 16; i++){
   let newDiv = document.createElement('div');
    newDiv.id = "box"+i;
    newDiv.className = "grid"
+   newDiv.addEventListener('mouseover', e => {
+    e.target.style.backgroundColor = "gray";
+   });
    toAdd.appendChild(newDiv);
 }
 document.querySelector('#container').appendChild(toAdd);
-
-
-/**
- * Add mouseover effect
- */
-addEventListener('mouseover', e => {
-  if(e.target.matches('.grid')) {
-    console.log(e.target.nodeName);
-    e.target.style.backgroundColor = "gray";
-  }
-});
