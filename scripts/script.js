fde42a0271d49a0e124ade4982f462c6dd4dc040
Alden Kim <aldenk51@gmail.com>
2023-03-09 23:26:14 -0500

    Added trail effect

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,22 @@
-var toAdd = document.createDocumentFragment();
-for(var i=0; i < 16; i++){
-   var newDiv = document.createElement('div');
+/**
+ * Create a grid of boxes
+ */
+let toAdd = document.createDocumentFragment();
+for(let i=0; i < 16; i++){
+  let newDiv = document.createElement('div');
    newDiv.id = "box"+i;
    newDiv.className = "grid"
    toAdd.appendChild(newDiv);
 }
+document.querySelector('#container').appendChild(toAdd);
 
-document.querySelector("#container").appendChild(toAdd);
 
+/**
+ * Add mouseover effect
+ */
+addEventListener('mouseover', e => {
+  if(e.target.matches('.grid')) {
+    console.log(e.target.nodeName);
+    e.target.style.backgroundColor = "gray";
+  }
+});
